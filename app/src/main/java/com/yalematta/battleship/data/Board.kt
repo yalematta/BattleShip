56eebbb75e0ef9a9c2a692cf862b93a51c067477
yalematta <layale@synkers.com>
2020-03-17 11:28:36 +0200

    Added description for Model parameters

--- app/src/main/java/com/yalematta/battleship/data/Board.kt
+++ app/src/main/java/com/yalematta/battleship/data/Board.kt
@@ -2,8 +2,10 @@ package com.yalematta.battleship.data
 
 class Board(var boardX: Int = 10, var boardY: Int = 10) {
 
+    // An ArrayList to store and keep track of ships, their points, names and status (hit or sunk)
     var fleet: ArrayList<Ship> = arrayListOf()
 
+    // 0: empty, not hit / 1: empty, but hit / 2: not empty, not hit / 3: not empty, but hit
     private var fieldStatus = Array(boardX) { _ -> Array(boardY) { _ -> 0} }
 
     fun getFieldStatusItem(x: Int, y: Int): Int {
--- app/src/main/java/com/yalematta/battleship/data/Ship.kt
+++ app/src/main/java/com/yalematta/battleship/data/Ship.kt
@@ -2,6 +2,7 @@ package com.yalematta.battleship.data
 
 class Ship(val shipType: ShipType, var orientation: Orientation = Orientation.VERTICAL) {
 
+    // An array list to store this ships coordinates
     var coords: ArrayList<Coordinate> = arrayListOf()
 
 }
