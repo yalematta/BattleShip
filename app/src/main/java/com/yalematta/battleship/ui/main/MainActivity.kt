a3aa8615abc47f4395726e1556f84e27b0bb10ce
yalematta <layale@synkers.com>
2020-03-19 09:08:13 +0200

    Added Logout functionality from MainActivity

--- app/src/main/java/com/yalematta/battleship/ui/main/MainActivity.kt
+++ app/src/main/java/com/yalematta/battleship/ui/main/MainActivity.kt
@@ -2,10 +2,16 @@ package com.yalematta.battleship.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseUser
 import com.yalematta.battleship.R
+import com.yalematta.battleship.data.firebase.FirebaseSource
 import com.yalematta.battleship.internal.getViewModel
 import com.yalematta.battleship.ui.login.LoginActivity
+import com.yalematta.battleship.ui.setup.SetupActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +24,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        logoutButton.setOnClickListener {
+
+            AuthUI.getInstance().signOut(this@MainActivity).addOnCompleteListener {
+                // user is now signed out
+                Toast.makeText(applicationContext, getString(R.string.user_signed_out), Toast.LENGTH_SHORT).show()
+                launchLoginActivity()
+            }
+        }
+
+        continueButton.setOnClickListener {
+            val intent = Intent(this, SetupActivity::class.java)
+            startActivity(intent)
+        }
+
         getStartedButton.setOnClickListener {
             launchLoginActivity()
         }
@@ -28,4 +48,31 @@ class MainActivity : AppCompatActivity() {
         this.startActivity(intent)
         finish()
     }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        updateUI(FirebaseSource().currentUser())
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+        if (currentUser != null) {
+
+            welcomeView.text = String.format("Welcome - %s", currentUser.displayName)
+            getStartedButton.visibility = View.GONE
+            logoutButton.visibility = View.VISIBLE
+            continueButton.visibility = View.VISIBLE
+
+        } else {
+
+            getStartedButton.visibility = View.VISIBLE
+            logoutButton.visibility = View.GONE
+            continueButton.visibility = View.GONE
+        }
+    }
 }
+
+
+
+
